a7fd3bfb2256aae2b96099ff805e0d0ce7b5fb0f
Mahmoud-harasis <mahmoudharasis8@gmail.com>
2023-03-12 16:53:44 +0300

    taske

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function changeBG() {
+    document.getElementById('btn-Js').style.backgroundColor = "#F53838";
+}
+function returnBG() {
+    document.getElementById('btn-Js').style.backgroundColor = "#000";
+}

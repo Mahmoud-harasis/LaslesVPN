e7fab928c02a462c3c456a93064c43461b3d1e3b
Mahmoud-harasis <mahmoudharasis8@gmail.com>
2023-03-13 01:24:20 +0300

    m

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function changeBG() {
-    document.getElementById('btn-Js').style.backgroundColor = "#F53838";
-}
-function returnBG() {
-    document.getElementById('btn-Js').style.backgroundColor = "#000";
-}
